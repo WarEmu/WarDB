f31bee5b7e17bbe875c0193dcf982329667daf94
nalgol <t.eckfeld-bergen@gmx.de>
2014-04-19 20:54:18 +0200

    all portals t1- t4 orde chaos by thorek

--- world/zone_jumps.sql
+++ world/zone_jumps.sql
@@ -1,3 +1,7 @@
+
+
+SET FOREIGN_KEY_CHECKS=0;
+
 DROP TABLE IF EXISTS `zone_jumps`;
 CREATE TABLE `zone_jumps` (
   `Entry` int(10) unsigned DEFAULT NULL,
@@ -13,5 +17,15 @@ CREATE TABLE `zone_jumps` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-INSERT INTO `test`.`zone_jumps` (`Entry`, `ZoneId`, `WorldX`, `WorldY`, `WorldZ`, `WorldO`, `zone_jumps_ID`) VALUES ('112037928', '107','1016470', '913114', '5624', '300', '112037928');
-INSERT INTO `test`.`zone_jumps` (`Entry`, `ZoneId`, `WorldX`, `WorldY`, `WorldZ`, `WorldO`, `zone_jumps_ID`) VALUES ('113081960', '106', '881137', '926088', '7368', '300', '113081960');
+INSERT INTO `zone_jumps` VALUES ('105745576', '101', '1017046', '840896', '13455', '3490', '105745576');
+INSERT INTO `zone_jumps` VALUES ('106784360', '100', '880086', '849131', '6914', '1004', '106784360');
+INSERT INTO `zone_jumps` VALUES ('106785128', '108', '1212778', '917245', '6906', '2918', '106785128');
+INSERT INTO `zone_jumps` VALUES ('107477864', '107', '1069659', '948334', '6592', '650', '107477864');
+INSERT INTO `zone_jumps` VALUES ('107478952', '103', '1409364', '758101', '1653', '3116', '107478952');
+INSERT INTO `zone_jumps` VALUES ('108148456', '102', '1271661', '860318', '14793', '1160', '108148456');
+INSERT INTO `zone_jumps` VALUES ('112037928', '107', '1016543', '913101', '5624', '2306', '112037928');
+INSERT INTO `zone_jumps` VALUES ('113081960', '106', '881112', '926051', '7359', '1544', '113081960');
+INSERT INTO `zone_jumps` VALUES ('113082280', '102', '1213491', '849745', '935', '14', '113082280');
+INSERT INTO `zone_jumps` VALUES ('113847016', '101', '1079434', '837268', '9704', '374', '113847016');
+INSERT INTO `zone_jumps` VALUES ('113858792', '109', '1412488', '918295', '828', '2788', '113858792');
+INSERT INTO `zone_jumps` VALUES ('115267048', '108', '1277638', '944633', '5984', '722', '115267048');
